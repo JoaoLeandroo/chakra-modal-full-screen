c096686a77dc12f11a0ecd90352e523e5d7d22a5
joaoleandro <new.joaoleandro@gmail.com>
2023-09-15 09:36:34 -0300

    add video youtube a modal

--- src/app/components/ModalGeneric/page.jsx
+++ src/app/components/ModalGeneric/page.jsx
@@ -44,7 +44,18 @@ export default function ModalGeneric(props) {
           <ModalHeader>{props.titulo}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            {props.conteudo}
+            <div className='w-full h-[500px] bg-yellow-400'>
+              {
+                <iframe 
+                className='w-full h-full'
+                src="https://www.youtube.com/embed/YC8SLpnxsL4?si=PcNmWbxRRYOoUvUy" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen>
+                </iframe>
+              }
+            </div>
           </ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>Close</Button>
